dec1e61e61ba5ec56299a543b00527cae1d06645
manbarmohamed <you@example.com>
2024-09-05 02:08:41 +0100

    modifier enum Categories et delete ReviewUpdateDto et ReviewWithClientDto

--- src/main/java/com/fil/rouge/emuns/Categories.java
+++ src/main/java/com/fil/rouge/emuns/Categories.java
@@ -2,20 +2,20 @@ package com.fil.rouge.emuns;
 
 public enum Categories {
 
-    WEB_DEVELOPMENT,     // Développement de sites web, applications web, et services backend
-    MOBILE_DEVELOPMENT,  // Développement d'applications mobiles pour iOS et Android
-    GRAPHIC_DESIGN,      // Design graphique, création de logos, illustrations, et autres éléments visuels
-    DIGITAL_MARKETING,   // Marketing digital, SEO, campagnes publicitaires, gestion des réseaux sociaux
-    CONTENT_WRITING,     // Rédaction de contenu, rédaction d'articles, blogs, et autres contenus écrits
-    VIDEO_EDITING,       // Montage vidéo, création de vidéos promotionnelles, animations
-    VIRTUAL_ASSISTANCE,  // Assistance virtuelle, gestion de tâches administratives, support client
-    TRANSLATION,         // Traduction de documents, localisation de contenus, interprétation
-    DATA_ANALYSIS,       // Analyse de données, création de rapports, visualisation de données
-    VOICE_OVER,          // Voix off pour vidéos, publicités, podcasts
-    UI_UX_DESIGN,        // Design d'interface utilisateur et expérience utilisateur pour applications et sites web
-    SOFTWARE_DEVELOPMENT,// Développement de logiciels personnalisés, systèmes embarqués, solutions desktop
-    ECOMMERCE,           // Création et gestion de boutiques en ligne, intégration de solutions e-commerce
-    BUSINESS_CONSULTING, // Conseil en affaires, stratégie d'entreprise, gestion de projets
-    AUDIO_PRODUCTION,    // Production audio, mixage, mastering, création de musique
+    WEB_DEVELOPMENT,
+    MOBILE_DEVELOPMENT,
+    GRAPHIC_DESIGN,
+    DIGITAL_MARKETING,
+    CONTENT_WRITING,
+    VIDEO_EDITING,
+    VIRTUAL_ASSISTANCE,
+    TRANSLATION,
+    DATA_ANALYSIS,
+    VOICE_OVER,
+    UI_UX_DESIGN,
+    SOFTWARE_DEVELOPMENT,
+    ECOMMERCE,
+    BUSINESS_CONSULTING,
+    AUDIO_PRODUCTION,
     PHOTOGRAPHY
 }
