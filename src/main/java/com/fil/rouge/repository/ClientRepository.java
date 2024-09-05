7748cc0af0cf29faa4fe3f001a6d75eaa93ab2d2
manbarmohamed <you@example.com>
2024-09-05 02:31:16 +0100

    ajouter la methode get client by username in ClientRepository

--- src/main/java/com/fil/rouge/repository/ClientRepository.java
+++ src/main/java/com/fil/rouge/repository/ClientRepository.java
@@ -4,4 +4,5 @@ import com.fil.rouge.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Client findByUsername(String username);
 }
