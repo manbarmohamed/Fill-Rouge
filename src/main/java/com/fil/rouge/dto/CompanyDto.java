2edecc76475fb77bc66e753ef6fe3137dcbbfb89
manbarmohamed <you@example.com>
2024-09-22 00:28:39 +0100

    Ajouter company entity et CompanyDto

---
+++ src/main/java/com/fil/rouge/dto/CompanyDto.java
@@ -0,0 +1,13 @@
+package com.fil.rouge.dto;
+
+import lombok.Data;
+
+@Data
+public class CompanyDto {
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private String website;
+    private String description;
+}
---
+++ src/main/java/com/fil/rouge/model/Company.java
@@ -0,0 +1,30 @@
+package com.fil.rouge.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private String website;
+    private String description;
+
+    @OneToMany(mappedBy = "company")
+    List<Client> clients;
+}
