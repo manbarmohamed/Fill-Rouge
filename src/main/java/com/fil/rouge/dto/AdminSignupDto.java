20beade18a1649b33c1cc4ff288459157cd4881a
manbarmohamed <you@example.com>
2024-09-22 00:16:32 +0100

    Ajouter  AdminSignupDto.java

---
+++ src/main/java/com/fil/rouge/dto/AdminSignupDto.java
@@ -0,0 +1,11 @@
+package com.fil.rouge.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AdminSignupDto extends UserDto{
+}
