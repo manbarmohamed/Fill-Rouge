fed8627e7ba2354ae83736a703995069f4084337
manbarmohamed <you@example.com>
2024-09-22 00:20:08 +0100

    Ajouter toApplicationDispalyDto methode in ApplicationMapper

--- src/main/java/com/fil/rouge/mapper/ApplicationMapper.java
+++ src/main/java/com/fil/rouge/mapper/ApplicationMapper.java
@@ -1,5 +1,6 @@
 package com.fil.rouge.mapper;
 
+import com.fil.rouge.dto.ApplicationDisplayDto;
 import com.fil.rouge.dto.ApplicationDto;
 
 import com.fil.rouge.model.Application;
@@ -18,6 +19,14 @@ public interface ApplicationMapper {
     @Mapping(source = "task.id", target = "taskId")
     @Mapping(source = "worker.id", target = "workerId")
     ApplicationDto toDto(Application application);
+
+    @Mapping(source = "task.title", target = "taskTitle")
+    @Mapping(source = "worker.name", target = "workerName")
+    ApplicationDisplayDto toDisplayDto(Application application);
+    @Mapping(source = "task.title", target = "taskTitle")
+    @Mapping(source = "worker.name", target = "workerName")
+    List<ApplicationDisplayDto> toDisplayDto(List<Application> application);
+
 
     List<Application> toEntity(List<ApplicationDto> applicationDto);
     List<ApplicationDto> toDto(List<Application> application);
