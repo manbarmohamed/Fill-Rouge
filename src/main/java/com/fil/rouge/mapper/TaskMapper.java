15043824ad84064e91ce52e3ddf22a8ba895a9d0
manbarmohamed <you@example.com>
2024-09-22 00:32:24 +0100

    Modifier TaskMapper

--- src/main/java/com/fil/rouge/mapper/TaskMapper.java
+++ src/main/java/com/fil/rouge/mapper/TaskMapper.java
@@ -9,15 +9,21 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TaskMapper {
 
-    // Mapping from Task entity to TaskDto including clientName and companyName
+    // ربط اسم العميل واسم الشركة بالـ TaskDto
     @Mapping(source = "client.name", target = "clientName")
-    @Mapping(source = "client.companyName", target = "companyName")
+    @Mapping(source = "client.company.name", target = "companyName")  // استخراج اسم الشركة من كيان Company المرتبط
+
+    @Mapping(source = "client.company.address", target = "address")  // استخراج اسم الشركة من كيان Company المرتبط
+    @Mapping(source = "client.company.phone", target = "phone")  // استخراج اسم الشركة من كيان Company المرتبط
+    @Mapping(source = "client.company.email", target = "email")  // استخراج اسم الشركة من كيان Company المرتبط
+    @Mapping(source = "client.company.website", target = "website")  // استخراج اسم الشركة من كيان Company المرتبط
+    @Mapping(source = "client.company.description", target = "descriptionCompany")  // استخراج اسم الشركة من كيان Company المرتبط
     TaskDto toDto(Task task);
 
     Task toEntity(TaskDto taskDto);
 
-    List<Task> toEntity(List<TaskDto> taskDto);
-    List<TaskDto> toDto(List<Task> task);
+    List<Task> toEntity(List<TaskDto> taskDtoList);
+    List<TaskDto> toDto(List<Task> taskList);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Task partialUpdate(TaskDto taskDto, @MappingTarget Task task);
