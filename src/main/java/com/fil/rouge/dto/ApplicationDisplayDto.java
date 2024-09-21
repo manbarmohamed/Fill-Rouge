c56b64a7ed629b62fa444124e329ef34f1117c0b
manbarmohamed <you@example.com>
2024-09-22 00:18:24 +0100

    Ajouter les methodes suivante dans ApplicationService ==>{ --getApplicationsByWorkerId --getApplicationsByTaskId --getApplicationsById }

---
+++ src/main/java/com/fil/rouge/dto/ApplicationDisplayDto.java
@@ -0,0 +1,18 @@
+package com.fil.rouge.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ApplicationDisplayDto {
+
+    private String taskTitle;
+    private String workerName;
+}
