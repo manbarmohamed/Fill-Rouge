1d5e33b4c0e79293dbe3a7377663c45af0e416cf
manbarmohamed <you@example.com>
2024-09-22 00:15:54 +0100

    modification de Admin.java

--- src/main/java/com/fil/rouge/model/Admin.java
+++ src/main/java/com/fil/rouge/model/Admin.java
@@ -2,9 +2,7 @@ package com.fil.rouge.model;
 
 
 import com.fil.rouge.emuns.Role;
-import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
