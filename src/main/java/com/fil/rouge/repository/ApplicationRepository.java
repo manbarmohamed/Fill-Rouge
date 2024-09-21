38c35545a9e9ca1d506b394954660d747ac3519c
manbarmohamed <you@example.com>
2024-09-22 00:22:42 +0100

    Ajouter les methodes suivante dans ApplicationRepository ==>{ --getApplicationsByWorkerId --getApplicationsByTaskId
    }

--- src/main/java/com/fil/rouge/repository/ApplicationRepository.java
+++ src/main/java/com/fil/rouge/repository/ApplicationRepository.java
@@ -1,7 +1,15 @@
 package com.fil.rouge.repository;
 
+import com.fil.rouge.dto.ApplicationDisplayDto;
+import com.fil.rouge.dto.ApplicationDto;
 import com.fil.rouge.model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+    List<Application> findApplicationsByTaskId(Long taskId);
+    List<Application> findApplicationsByWorkerId(Long taskId);
+
 }
